a8ddcf8706d2139ec5225218597cf47c1b427a37
Gary Margolis <gsmargolis@outlook.com>
2016-09-07 00:52:29 +0000

    test

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-    before_action :require_user, except: [:home]
+    #before_action :require_user, except: [:home]
     
   def importplayers
     require 'csv'
