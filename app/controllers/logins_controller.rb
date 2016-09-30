96b203be8bd4c4aa1163aab5860958396f5e52f0
Gary Margolis <gsmargolis@outlook.com>
2016-09-30 20:10:04 +0000

    Add special logon message about the tbowl account

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -21,7 +21,11 @@ class LoginsController < ApplicationController
     else
     # If user's login doesn't work, send them back to the login form.
       redirect_to '/login'
-      flash[:danger] = "Incorrect Username or Password"
+      if params[:email] == "tbowl"
+        flash[:danger] = "The \"tbowl\" account is no longer available.\nPlease click the \"Create New Account\" link."
+      else
+        flash[:danger] = "Incorrect Username or Password"
+      end
     end
   end
 
