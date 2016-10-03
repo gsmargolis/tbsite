f52075620ae3a52db93d48b2bb73a8dea5d99930
Gary Margolis <gsmargolis@outlook.com>
2016-10-03 02:14:03 +0000

    fixed bug in current user helper routine

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
   
   
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
     (@current_user != nil)? @current_player = Player.find_by(cbsid: @current_user.cbs_id) : nil
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   
   def logged_in?
