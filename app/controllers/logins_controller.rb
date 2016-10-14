92406d0bf37b78f8a0f361aa71debedca284b664
Gary Margolis <gsmargolis@outlook.com>
2016-10-14 00:13:29 +0000

    made logon username case insensitive

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -4,7 +4,9 @@ class LoginsController < ApplicationController
   end
 
   def create
-    user = User.find_by_username(params[:username])
+    #user = User.find_by_username(params[:username])
+    user = User.find_by("lower(username) = ?", params[:username].downcase)
+
     # If the user exists AND the password entered is correct.
     if user && user.authenticate(params[:password])
       # Save the user id inside the browser cookie. This is how we keep the user 
